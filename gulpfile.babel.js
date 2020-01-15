a4998fafaafa15ea8c7b817945ca24ec730cd811
Jay Kang <kyj8028@hotmail.com>
2020-01-15 23:59:43 +0900

    gulp tutorial - 2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,30 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver"
+
+const routes = {
+    pug: {
+        watch: "src/**/*.pug",
+        src: "src/*.pug",
+        dest: "build"
+    }
+};
+
+const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+const clean = () => del(["build"]);
+
+const webserver = () => gulp.src("build").pipe(ws({livereload:true, open:true}));
+
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+}
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+const postDev = gulp.parallel([webserver, watch]); // 동시에 실행시키고 싶으면 parallel, 그렇지 않으면 series
+
+export const dev = gulp.series([prepare, assets, postDev]);
